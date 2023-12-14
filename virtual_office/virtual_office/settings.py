55c0cf1d81104b342541656d297858247e965be3
Alexey <al913@mail.ru>
2023-12-15 02:48:13 +0300

    z

--- virtual_office/virtual_office/settings.py
+++ virtual_office/virtual_office/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7c3(r-@+3g-nsd(!cly&46w#9!t&^nv&4--_gk%gzf=vd&@*!g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0',
                  'localhost',
